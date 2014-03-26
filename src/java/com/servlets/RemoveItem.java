bd8b027e6af032c2c91b2a444f910e31e7b5ca0f
KimOoO <KimOoO@KimOoO-PC>
2014-03-26 06:31:00 +0200

    Kareem: Updating removeitem

--- src/java/com/servlets/RemoveItem.java
+++ src/java/com/servlets/RemoveItem.java
@@ -4,8 +4,11 @@
  */
 package com.servlets;
 
+import com.dal.dao.ItemHome;
+import com.dal.pojo.Item;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,15 +35,12 @@ public class RemoveItem extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RemoveItem</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RemoveItem at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           int itemId = Integer.parseInt(request.getParameter("itemId"));
+        ItemHome itemhome = new ItemHome();
+        Item currentItem = itemhome.findById(itemId);
+        itemhome.delete(currentItem);
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/FurnitureCrazeV1-1/LoadCategoryItems?category=BedRooms");
+        dispatcher.forward(request, response);
         } finally {            
             out.close();
         }
