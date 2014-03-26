ec3b26db42b5747a7b8e19ec1c493b88306836ad
KimOoO <KimOoO@KimOoO-PC>
2014-03-26 06:07:48 +0200

    Kareem: AddNewItem.java

--- src/java/com/servlets/AddNewItem.java
+++ src/java/com/servlets/AddNewItem.java
@@ -4,8 +4,13 @@
  */
 package com.servlets;
 
+import com.dal.dao.CategoriesHome;
+import com.dal.dao.ItemHome;
+import com.dal.pojo.Categories;
+import com.dal.pojo.Item;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +21,11 @@ import javax.servlet.http.HttpServletResponse;
  * @author Yassmeen
  */
 public class AddNewItem extends HttpServlet {
+
+    private final static int KITCHENS= 1;
+    private final static int DiningRooms= 2;
+    private final static int BedRooms= 3; 
+    private final static int LivingRooms= 4; 
 
     /**
      * Processes requests for both HTTP
@@ -33,14 +43,42 @@ public class AddNewItem extends HttpServlet {
         PrintWriter out = response.getWriter();
         try {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AddNewProduct</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AddNewProduct at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           Item item = new Item();
+            ItemHome ih = new ItemHome();
+            CategoriesHome ch = new CategoriesHome();
+            
+            String name = request.getParameter("name");
+            String AvQuant = request.getParameter("email");
+            String price = request.getParameter("address");
+            String cat = request.getParameter("categories");
+            
+            String des = request.getParameter("interests");
+            item.setAvailableQuantity(Integer.parseInt(AvQuant)); 
+            if(cat.toLowerCase().equals("kitchens")){
+                Categories c = ch.findById(KITCHENS);
+                item.setCategories(c);
+            }
+            if(cat.toLowerCase().equals("diningrooms")){
+                Categories c = ch.findById(DiningRooms);
+                item.setCategories(c);
+            }            
+            if(cat.toLowerCase().equals("bedrooms")){
+                Categories c = ch.findById(BedRooms);
+                item.setCategories(c);
+            }
+            if(cat.toLowerCase().equals("livingrooms")){
+                Categories c = ch.findById(LivingRooms);
+                item.setCategories(c);
+            }
+            item.setDescription(des);
+            item.setName(name);
+            item.setPrice(Double.parseDouble(price));
+            
+            ih.merge(item);
+            
+            response.sendRedirect("/FurnitureCrazeV1-1/Admin.jsp?date=" + new Date().getTime());
+            
+            System.out.println(name + " " + AvQuant + " " + price + " " + cat );
         } finally {            
             out.close();
         }
